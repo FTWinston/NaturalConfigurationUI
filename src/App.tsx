8f8f829b64aa9de102d3fec6fd2c7b041873e269
FTWinston <ftwinston@gmail.com>
2019-10-07 00:03:35 +0100

    Attempting to actually use UI with a real parser

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,67 @@ import * as React from 'react';
 import './App.css';
 import { ConfigTextArea } from './ui';
 import { useState } from 'react';
-import { IParserError } from 'natural-configuration';
+import { IParserError, ConfigurationParser } from 'natural-configuration';
 
-const errors = [
+interface IString {
+    value: string;
+}
+
+const parser = new ConfigurationParser<IString>([
     {
-        message: 'hello',
-        startIndex: 6,
-        length: 5,
+        type: 'standard',
+        expressionText: 'Replace \"(.*)\" with \"(.*)\"',
+        parseMatch: (modify, match) => {
+            if (match[1].length === 0) {
+                return [{
+                    startIndex: 8,
+                    length: 2,
+                    message: 'Match text cannot be empty.'
+                }];
+            }
+        
+            const before = new RegExp(match[1], 'g');
+            const after = match[2];
+        
+            modify.value = modify.value.replace(before, after);
+            return [];
+        },
+        examples: [
+            'Replace "x" with "y"',
+            'Replace "something" with ""'
+        ]
+    },
+    {
+        type: 'standard',
+        expressionText: 'Convert to (.+) case',
+        parseMatch: (modify, match) => {
+            if (match[1] === 'upper') {
+                modify.value = modify.value.toUpperCase();
+            }
+            else if (match[1] === 'lower') {
+                modify.value = modify.value.toLowerCase();
+            }
+            else {
+                return [{
+                    startIndex: 11,
+                    length: match[1].length,
+                    message: `Unexpected case value: ${match[1]}`
+                }];
+            }
+    
+            return [];
+        },
+        examples: [
+            'Convert to upper case',
+            'Conver to lower case'
+        ]
     }
-];
+]);
 
 const App = () => {
     const [text, setText] = useState('');
+
+    const [errors, setErrors] = useState([] as IParserError[]);
 
     const [highlightedError, setSelectedError] = useState(undefined as IParserError | undefined);
 
@@ -21,7 +70,10 @@ const App = () => {
         <>
             <ConfigTextArea
                 text={text}
-                onChange={newText => setText(newText)}
+                onChange={newText => {
+                    setText(newText);
+                    setErrors(parser.parse({value: 'Test value to modify'}, newText));
+                }}
                 errors={errors}
                 highlightedError={highlightedError}
                 onEnterError={err => setSelectedError(err)}
